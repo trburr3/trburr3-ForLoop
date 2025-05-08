209db36e1732586822aedca5e6022c56c692ce0a
Tre B <burrowstrevon@gmail.com>
2025-05-08 03:50:45 +0000

    completed lab

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -44,6 +44,9 @@ public class Lab {
         String buildingString = "";
 
 //        code here
+        for (int i = start; i < end; i++){
+            buildingString = buildingString + i + " ";
+        }
 
         return buildingString;
     }
